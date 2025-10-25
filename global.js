3fd3dd6758cdcbedc49861f1e13084e4546700e9
Amelia Wong <amw013@ucsd.edu>
2025-10-24 18:55:20 -0700

    making navbar using js

--- global.js
+++ global.js
@@ -1,14 +1,38 @@
-
 function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
 
-const navLinks = $$('nav a');
+let pages = [
+    { url: '', title: 'Home' },
+    { url: 'projects/', title: 'Projects' },
+    { url: 'contact/', title: 'Contact' },
+    { url: 'resume/', title: 'Resume' },
+    { url: 'https://github.com/amw013', title: 'GitHub' } 
+];
 
-let currentLink = navLinks.find(
-  (a) => a.host === location.host && a.pathname === location.pathname
-);
+const BASE_PATH = (location.hostname === "localhost" || location.hostname === "127.0.0.1")
+  ? "/"                  
+  : "/portfolio/";       
 
-currentLink?.classList.add('current');
 
-console.log(currentLink);
+let nav = document.createElement('nav');
+document.body.prepend(nav);
+
+for (let p of pages) {
+    let url = p.url.startsWith('http') ? p.url : BASE_PATH + p.url;
+
+    let a = document.createElement('a');
+    a.href = url;
+    a.textContent = p.title;
+
+    a.classList.toggle(
+        'current',
+        a.host === location.host && a.pathname === location.pathname
+    );
+
+    if (a.host !== location.host) {
+        a.target = "_blank";
+    }
+
+    nav.append(a);
+}
