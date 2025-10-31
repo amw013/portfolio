d5936cdb2158adef4ef2d1c13e56a4a6bb689ed4
Amelia Wong <amw013@ucsd.edu>
2025-10-31 00:01:46 -0700

    fixing pie chart

--- projects/projects.js
+++ projects/projects.js
@@ -11,21 +11,18 @@ renderProjects(projects, projectsContainer, 'h2');
 
 import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
 
-let arcGenerator = d3.arc().innerRadius(0).outerRadius(50);
-let arc = arcGenerator({ startAngle: 0, endAngle: 2 * Math.PI });
-d3.select('svg').append('path').attr('d', arc).attr('fill', 'red');
-
 let data = [1, 2, 3, 4, 5, 5];
 
+let arcGenerator = d3.arc().innerRadius(0).outerRadius(50);
 
 let sliceGenerator = d3.pie();
-const arcData = sliceGenerator(data); 
-const arcs = arcData.map(d => arcGenerator(d));
+const arcData = sliceGenerator(data);
 
 let colors = d3.scaleOrdinal(d3.schemeTableau10);
 
-arcs.forEach((arc, idx) => {
+arcData.forEach((d, idx) => {
   d3.select('#projects-pie-plot')
-    .attr('d', arc)
+    .append('path')
+    .attr('d', arcGenerator(d))
     .attr('fill', colors(idx));
 });
