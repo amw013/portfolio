3fc58eaa1103bf583cd11d5e875aa32e8f344334
Amelia Wong <amw013@ucsd.edu>
2025-10-31 01:39:54 -0700

    Update projects.js

--- projects/projects.js
+++ projects/projects.js
@@ -37,10 +37,10 @@ arcs.forEach((arc, idx) => {
 });
 
 let legend = d3.select('.legend');
-
 data.forEach((d, idx) => {
-  legend.append('li')
-    .attr('style', `--color:${colors(idx)}`) // store the slice color in a CSS variable
-    .html(`<span class="swatch"></span> ${d.label} <em>(${d.value})</em>`)
-    .attr('class', 'legend-item');
+  legend
+    .append('li')
+    .attr('style', `--color:${colors(idx)}`) // set the style attribute while passing in parameters
+    .attr('class', 'legend-item')
+    .html(`<span class="swatch"></span> ${d.label} <em>(${d.value})</em>`); // set the inner html of <li>
 });
