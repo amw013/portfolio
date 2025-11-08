dbbdc544c51cdae75a90db024ba3e33e33a10a20
Amelia Wong <amw013@ucsd.edu>
2025-11-07 22:14:35 -0800

    displaying stats

--- meta/main.js
+++ meta/main.js
@@ -13,7 +13,6 @@ async function loadData() {
   return data;
 }
 
-let data = await loadData();
 
 function processCommits(data) {
   return d3
@@ -46,6 +45,44 @@ function processCommits(data) {
     });
 }
 
-let commits = processCommits(data);
+function renderCommitInfo(data, commits) {
+  const dl = d3.select('#stats').append('dl').attr('class', 'stats');
 
-console.log(commits);
+  dl.append('dt').html('Total <abbr title="Lines of code">LOC</abbr>');
+  dl.append('dd').text(data.length);
+
+  dl.append('dt').text('Total commits');
+  dl.append('dd').text(commits.length);
+
+  const numFiles = d3.groups(data, d => d.file).length;
+  dl.append('dt').text('Number of files');
+  dl.append('dd').text(numFiles);
+
+  const fileLengths = d3.rollups(
+    data,
+    v => d3.max(v, d => d.line),
+    d => d.file
+  );
+  const longestFile = d3.greatest(fileLengths, d => d[1])?.[0];
+  dl.append('dt').text('Longest file');
+  dl.append('dd').text(longestFile);
+
+  const avgLineLength = d3.mean(data, d => d.length);
+  dl.append('dt').text('Average line length (chars)');
+  dl.append('dd').text(avgLineLength.toFixed(1));
+
+  const workByPeriod = d3.rollups(
+    data,
+    v => v.length,
+    d => new Date(d.datetime).toLocaleString('en', { dayPeriod: 'short' })
+  );
+  const maxPeriod = d3.greatest(workByPeriod, d => d[1])?.[0];
+  dl.append('dt').text('Most active time of day');
+  dl.append('dd').text(maxPeriod);
+}
+
+
+let data = await loadData();
+let commits = processCommits(data);
+renderCommitInfo(data, commits);
+
